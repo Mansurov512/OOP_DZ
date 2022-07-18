5a19b6ebd1c3e784f8095b8c0f2925ec31d331a8
Mansurov512 <do9jng@gmail.com>
2022-07-18 15:58:38 +0300

    Сделал первую часть ДЗ

---
+++ Lesson02/BankScore/BankScore/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Lesson02;
+
+
+    BankAccount firstAccount = new BankAccount();
+
+firstAccount.SetNumberOfAccount(3216_5498_7302_2345);
+firstAccount.SetBalanceOfAccount(1_000_000);
+firstAccount.SetAccountType(BankAccount.AccountType.Crypt);
+
+
+
+Console.WriteLine($"Номер вашего счёта: {firstAccount.GetNumberOfAccount()} " +
+                $"\nБаланс вашего счёта: {firstAccount.GetBalanceOfAccount()} тугриков" +
+                $"\nТип вашего счёта: {firstAccount.GetAccountType()}");
+
+
+
+Console.ReadKey(true);
+
+
+
---
+++ Lesson02/BankScore/BankScore/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using BankScore;
+
+
+namespace Lesson02
+{
+
+    public class BankAccount
+    {
+
+        private long _numberOfAccount;
+        private decimal _balanceOfAccount;
+        public enum AccountType//enum не сделать приватным, иначе не работает программа
+        {
+            Debit, Credit, Brokerage, Crypt
+        }
+        private AccountType _accountType;//потому создаём поле/переменную на базе созданного enum
+                                         //и уже она становится преватной
+        public void SetAccountType(AccountType accountType)//выводит значения из enum
+            //можно было бы добавить сообщения о типе счёта на русском
+        {
+
+            switch (accountType)
+            {
+                case AccountType.Debit:
+                    _accountType = AccountType.Debit;
+                    break;
+
+                case AccountType.Credit:
+                    _accountType = AccountType.Credit;
+                    break;
+
+                case AccountType.Brokerage:
+                    _accountType = AccountType.Brokerage;
+                    break;
+
+                case AccountType.Crypt:
+                    _accountType = AccountType.Crypt;
+                    break;
+
+                    //default:
+                    //throw new ArgumentException("нет такого типа");
+
+            }
+
+
+        }
+
+        public AccountType GetAccountType()
+        {
+            return _accountType;
+
+        }
+        public void SetNumberOfAccount(long a)
+        {
+            _numberOfAccount = a;
+
+        }
+
+        public long GetNumberOfAccount()
+        {
+            return _numberOfAccount;
+        }
+
+        public void SetBalanceOfAccount(decimal b)
+        {
+            _balanceOfAccount = b;
+        }
+        public decimal GetBalanceOfAccount()
+        {
+            return _balanceOfAccount;
+        }
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
