ef784135c7aa38a4efae2989d1d4bbafeeba308b
Mansurov512 <do9jng@gmail.com>
2022-07-18 18:46:52 +0300

    Сделал 1, 2 и 4ые задания из ДЗ. Во 2ом номер счёта может быть не уникальным, см. комментарий в коде.

--- Lesson02/BankScore/BankScore/BankAccount.cs
+++ Lesson02/BankScore/BankScore/BankAccount.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using BankScore;
+
 
 
 namespace Lesson02
@@ -12,15 +12,15 @@ namespace Lesson02
     public class BankAccount
     {
 
-        private long _numberOfAccount;
-        private decimal _balanceOfAccount;
+        private long _numberOfAccount;//номер счёта
+        private decimal _balanceOfAccount;//баланс
         public enum AccountType//enum не сделать приватным, иначе не работает программа
         {
             Debit, Credit, Brokerage, Crypt
         }
-        private AccountType _accountType;//потому создаём поле/переменную на базе созданного enum
-                                         //и уже она становится преватной
-        public void SetAccountType(AccountType accountType)//выводит значения из enum
+        private AccountType _accountType;//потому для типа счёта создаём поле/переменную на базе созданного enum
+                                         //и уже она становится приватной
+        public void SetAccountType(AccountType accountType)//сеттер типа счёта, устанавливает значения из enum
             //можно было бы добавить сообщения о типе счёта на русском
         {
 
@@ -44,33 +44,33 @@ namespace Lesson02
 
                     //default:
                     //throw new ArgumentException("нет такого типа");
-
+                    //так как вводим значения в сами методы в коде, если я верно понял задание,
+                    //то при неверном вводе программа просто не скомпелируется, потому нет смысла в default
             }
-
 
         }
 
-        public AccountType GetAccountType()
+        public AccountType GetAccountType()//геттер типа банковского счёта
         {
             return _accountType;
 
         }
-        public void SetNumberOfAccount(long a)
+        public void SetNumberOfAccount(long a)//сеттер номера  счёта
         {
             _numberOfAccount = a;
 
         }
 
-        public long GetNumberOfAccount()
+        public long GetNumberOfAccount()//геттер номера  счёта
         {
             return _numberOfAccount;
         }
 
-        public void SetBalanceOfAccount(decimal b)
+        public void SetBalanceOfAccount(decimal b) //сеттер баланса
         {
             _balanceOfAccount = b;
         }
-        public decimal GetBalanceOfAccount()
+        public decimal GetBalanceOfAccount() //геттер баланса
         {
             return _balanceOfAccount;
         }
---
+++ Lesson02/BankScore/BankScore/BankAccount4.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Lesson02_4
+{
+
+    public class BankAccount4
+    {
+
+        private int _numberOfAccount;//номер счёта
+        private decimal _balanceOfAccount;//баланс
+        public enum AccountType//enum не сделать приватным, иначе не работает программа
+        {
+            Debit, Credit, Brokerage, Crypt
+        }
+        private AccountType _accType;//потому для типа счёта создаём поле/переменную на базе созданного enum
+                                         //и уже она становится приватной
+
+        public int NumberOfAccount
+        {
+            get { return _numberOfAccount; }
+            set { _numberOfAccount = value; }
+        }
+
+        public decimal BalanceOfAccount
+        {
+            get { return _balanceOfAccount; }
+            set { _balanceOfAccount = value; }
+        }
+
+        public AccountType AccType
+        {
+            get { return _accType; }
+            set { _accType = value; }
+
+        }
+
+        
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
