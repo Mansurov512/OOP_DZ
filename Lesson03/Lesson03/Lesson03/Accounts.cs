e470eb90b78734a08001f9a3d7ea342b1bf949b9
Mansurov512 <do9jng@gmail.com>
2022-07-20 22:30:09 +0300

    Почти сделал первое задание

---
+++ Lesson03/Lesson03/Lesson03/Accounts.cs
@@ -0,0 +1,66 @@
+﻿using Lesson02_4;
+
+
+//BankAccount firstAccount = new BankAccount();//объект для первого задания
+
+//firstAccount.SetNumberOfAccount(3216_5498_7302_2345);
+//firstAccount.SetBalanceOfAccount(1_000_000);
+//firstAccount.SetAccountType(BankAccount.AccountType.Crypt);
+
+
+
+//Console.WriteLine($"Номер вашего счёта: {firstAccount.GetNumberOfAccount()} " +
+//                $"\nБаланс вашего счёта: {firstAccount.GetBalanceOfAccount()} койнов" +
+//                $"\nТип вашего счёта: {firstAccount.GetAccountType()}\n");
+
+
+//BankAccount2 secondAccount = new BankAccount2();//объект для второго задания
+
+//secondAccount.SetNumberOfAccount();
+//secondAccount.SetBalanceOfAccount(5_000_000);
+//secondAccount.SetAccountType(BankAccount2.AccountType.Brokerage);
+
+
+
+//Console.WriteLine($"Номер вашего счёта: {secondAccount.GetNumberOfAccount()} " +
+//                $"\nБаланс вашего счёта: {secondAccount.GetBalanceOfAccount()} златых" +
+//                $"\nТип вашего счёта: {secondAccount.GetAccountType()}\n");
+
+
+
+//BankAccount4 fourthAccount = new BankAccount4();//объект для четвёртого задания
+
+//fourthAccount.NumberOfAccount = 123;
+//fourthAccount.BalanceOfAccount = 500;
+//fourthAccount.AccType = BankAccount4.AccountType.Debit;
+
+
+//Console.WriteLine($"Номер вашего счёта: {fourthAccount.NumberOfAccount}" +
+//                  $"\nБаланс вашего счёта: {fourthAccount.BalanceOfAccount} тугриков" +
+//                  $"\nТип вашего счёта: {fourthAccount.AccType}\n");
+
+
+
+BankAccount4 Acc1 = new();
+Acc1.NumberOfAccount = 1111;
+Acc1.BalanceOfAccount = 1_000;
+Acc1.AccType = BankAccount4.AccountType.Debit;
+
+BankAccount4 Acc2 = new();
+Acc2.NumberOfAccount = 2222;
+Acc2.BalanceOfAccount = 2_000;
+Acc2.AccType = BankAccount4.AccountType.Debit;
+
+
+
+Acc1.TransferMoneyFromMeTo(Acc2, 1000);
+
+
+
+
+
+
+Console.ReadKey(true);
+
+
+
---
+++ Lesson03/Lesson03/Lesson03/BankAccount4.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson02_4
+{
+
+    public class BankAccount4
+    {
+
+        private int _numberOfAccount;//номер счёта
+        private decimal _balanceOfAccount;//баланс
+        public enum AccountType//enum не сделать приватным, иначе не работает программа
+        {
+            Debit, Credit, Brokerage, Crypt
+        }
+        private AccountType _accType;//потому для типа счёта создаём поле/переменную на базе созданного enum
+                                     //и уже она становится приватной
+
+        public int NumberOfAccount
+        {
+            get { return _numberOfAccount; }
+            set { _numberOfAccount = value; }
+        }
+
+        public decimal BalanceOfAccount
+        {
+            get { return _balanceOfAccount; }
+            set { _balanceOfAccount = value; }
+        }
+
+        public AccountType AccType
+        {
+            get { return _accType; }
+            set { _accType = value; }
+
+        }
+        /// <summary>
+        /// Перевод средств со счёта на счёт
+        /// </summary>
+        /// <param name="AccTo">Счёт(объект) НА который будет перевод</param>
+        /// <param name="Amount">Сумма перевода</param>
+        /// <returns></returns>
+        public bool TransferMoneyFromMeTo(BankAccount4 AccTo, decimal Amount)
+        {
+            if ((this.AccType == AccTo.AccType) && (this.BalanceOfAccount >= Amount))
+            {
+                this.BalanceOfAccount -= Amount;
+                AccTo.BalanceOfAccount += Amount;
+                Console.WriteLine($"Со счёта {this} списано {Amount} у.е." +
+                    $"\nБаланс {this} составляет {this.BalanceOfAccount}." +
+                    $"\nБаланс {AccTo} составляет {AccTo.BalanceOfAccount}.");
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Баланс недостаточен или типы счетов не совпадают.");
+                return false;
+            }
+
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
