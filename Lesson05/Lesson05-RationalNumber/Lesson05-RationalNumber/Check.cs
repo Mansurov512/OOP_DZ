9eff46db090aec8d1029193bfa8254c4f66aa58d
Mansurov512 <do9jng@gmail.com>
2022-08-01 14:25:01 +0300

    Первая фиксация

--- Lesson05/Lesson05-RationalNumber/Lesson05-RationalNumber/Check.cs
+++ Lesson05/Lesson05-RationalNumber/Lesson05-RationalNumber/Check.cs
@@ -3,13 +3,13 @@
 RationalNumber r1 = new (50, 2);
 RationalNumber r2 = new (10, 100);
 
-RationalNumber r3 = r1 + r2;
+//RationalNumber r3 = r1 + r2;
 
 
 
 
 Console.WriteLine(r1.ToString());//без ToString выводит десятичной дробью
-Console.WriteLine(r2.ToString());
-Console.WriteLine(r3.ToString());
+Console.WriteLine(r2);
+//Console.WriteLine(r3);
 
 Console.ReadKey(true);
--- Lesson05/Lesson05-RationalNumber/Lesson05-RationalNumber/RationalNumber.cs
+++ Lesson05/Lesson05-RationalNumber/Lesson05-RationalNumber/RationalNumber.cs
@@ -3,9 +3,9 @@
     public struct RationalNumber
     {
 
-        private int _numerator;// Числитель
-        private int _denominator;// Знаменатель
-        private double Rational => (double)_numerator / (double)_denominator;
+        public int _numerator;// Числитель
+        public int _denominator;// Знаменатель
+        public double Rational => (double)_numerator / (double)_denominator;
 
         public int Numerator { get => _numerator; }
         public int Denominator { get => _denominator; }
@@ -24,99 +24,24 @@
             _numerator = Numerator;
             _denominator = Denominator;
 
-            ReduceToLowestTerms();
+            // ReduceToLowestTerms();
 
         }
 
         public static implicit operator double(RationalNumber r1) => r1.Rational;
         public static implicit operator RationalNumber(int r1) => new RationalNumber(r1, 1);
 
-        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
-        {
+        //public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        //{
 
-            return new RationalNumber((r1.Numerator * r2.Denominator)
-            + (r2.Numerator * r1.Denominator), r1.Denominator * r2.Denominator);
-        }
-
-        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
-        {
-            return new RationalNumber((r1.Numerator * r2.Denominator)
-                - (r2.Numerator * r1.Denominator), r1.Denominator * r2.Denominator);
-        }
-
-        public static RationalNumber operator ++(RationalNumber numb)
-        {
-            return new RationalNumber(numb.Numerator + 1, numb.Denominator + 1);
-        }
-
-        public static RationalNumber operator --(RationalNumber numb)
-        {
-            return new RationalNumber(numb.Numerator - 1, numb.Denominator - 1);
-        }
-
-        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
-        {
-            return new RationalNumber(r1.Numerator * r2.Numerator, r1.Denominator * r2.Denominator);
-        }
-
-        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
-        {
-            return new RationalNumber(r1.Numerator * r2.Denominator, r1.Denominator * r2.Numerator);
-        }
+        //    return new RationalNumber((r1.Numerator * r2.Denominator)
+        //    + (r2.Numerator * r1.Denominator), r1.Denominator * r2.Denominator);
+        //}
 
 
-        public static bool operator >(RationalNumber r1, RationalNumber r2)
-        {
-            return (r1 - r2).Numerator > 0;
-        }
-        public static bool operator <(RationalNumber r1, RationalNumber r2)
-        {
-            return (r1 - r2).Numerator < 0;
-        }
-        public static bool operator ==(RationalNumber r1, RationalNumber r2)
-        {
-             return Equals(r1, r2);
-        }
-        public static bool operator <=(RationalNumber r1, RationalNumber r2)
-        {
-            return !(r1 > r2);
-        }
-        public static bool operator >=(RationalNumber r1, RationalNumber r2)
-        {
-            return !(r1 < r2);
-        }
-        public static bool operator !=(RationalNumber r1, RationalNumber r2)
-        {
-            return !(r1 == r2);
-        }
-                      
-        private void ReduceToLowestTerms()
-        {
-            int greatestCommonDivisor = RationalNumber.GetGCD(_numerator, _denominator);
-            _numerator /= greatestCommonDivisor;
-            _denominator /= greatestCommonDivisor;
-        }
-        private static int GetGCD(int term1, int term2)
-        {
-            if (term2 == 0)
-                return term1;
-            else
-                return GetGCD(term2, term1 % term2);
-        }
-        public override bool Equals(object obj)
-        {
-            if (!(obj is RationalNumber))
-            {
-                return false;
-            }
-
-            var number = (RationalNumber)obj;
-            return _numerator == number.Numerator &&
-                   _denominator == number.Denominator;
-        }
         public override string ToString()
         {
-            return $"{this._numerator}/{this._denominator}";
+            return $"{_numerator}/{_denominator}";
         }
     }
 }
